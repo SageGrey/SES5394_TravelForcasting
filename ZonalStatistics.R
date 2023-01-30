9eb9b2e32e70f057bba91344f735a6fb2ad4e742
Your Namesage.gray4@gmail.com <sage.gray4@gmail.com>
2023-01-30 17:02:33 -0500

    Adding more variables and starting to make maps

--- ZonalStatistics.R
+++ ZonalStatistics.R
@@ -1,15 +1,16 @@
 
 #Load Libraries
-# uwu
-
 library(tidyverse)
 library(tidycensus)
 library(tigris)
 library(sf)
-library(ggplot)
+library(ggplot2)
+library(hrbrthemes)
+library(devtools)
+library(nationalparkcolors)
 
 
-# Download longitudinal employer-household dynamica dataset
+# Download longitudinal employer-household dynamic dataset
 lehd_blocks <- read_csv('https://lehd.ces.census.gov/data/lodes/LODES7/ok/wac/ok_wac_S000_JT00_2015.csv.gz', show_col_types=FALSE) %>%
   rename(total_emp = C000) %>%
   mutate(basic_emp = CNS01+CNS02+CNS03+CNS04+CNS05+CNS06+CNS08+CNS09) %>%
@@ -26,12 +27,30 @@ lehd_tracts <- lehd_blocks %>%
   summarize(across(everything(), ~sum(.)))
 
 # Download census tract data
-hh_vars = c(no_veh = "B08201_002",
+hh_vars = c(
+            ## Number of Vehicles
+            no_veh = "B08201_002",
+            total_veh = "B08201_002",
+            one_veh = "B08201_003", 
+            two_veh = "	B08201_004",
+            three_veh = "	B08201_005",
+            fourplus_veh = "B08201_006",
+            
+            ## Total Households
             total_hhs = "B08201_001",
+            
+            ## Household Structure 
             hh_1person = "B08201_007",
             hh_2person = "B08201_013",
             hh_3person = "B08201_019",
             hh_4person_plus = "B08201_025",
+            
+            ## LifeCycle State
+            total_under6 = "B05009_002",
+            total_6to17= "B05009_020",
+            
+            ## Income
+            total_household_income = "B19001_001",
             inc_lt_10k = "B19001_002",
             inc_btw_10k_15k = "B19001_003",
             inc_btw_15k_20k = "B19001_004",
@@ -62,7 +81,69 @@ areas <- tracts(state = "OK",
   select(GEOID, ALAND) %>%
   st_drop_geometry()
 
+## Join Datasets 
 zones <- left_join(census, lehd_tracts) %>%
   left_join(areas)
 
+
+
+
+### Chloropleths
+
+pal <- park_palette("MtRainier")
+
+hh_size_map <- ggplot(zones) +
+  geom_sf(aes(fill = total_hhsE), color=NA) + 
+  ggtitle("Number of Households") +
+  theme_ipsum() +
+  theme(plot.title = element_text(size=12))
+hh_size_map
+
+
+total_employment_map <- ggplot(zones) +
+  geom_sf(aes(fill = total_emp), color=NA) + 
+  ggtitle("Total Employment") +
+  theme_ipsum() +
+  theme(plot.title = element_text(size=12))
+total_employment_map
+
+
+hh_size_map
+### Historgrams
+hist_hhs <- zones %>%
+  ggplot( aes(x=total_hhsE)) +
+  geom_histogram( binwidth=30, fill="#69b3a2", color="#e9ecef", alpha=0.9) +
+  ggtitle("Census Tracts with 'X' Num. of Households") +
+  xlab("Number of Households (Estimated)") +
+  ylab("# of Census Tracts") +
+  theme_ipsum() +
+  theme(
+    plot.title = element_text(size=12)
+  )
+hist_hhs
+
+
+Hist_NoVehAvail <- zones %>%
+  ggplot( aes(x=no_vehE)) +
+  geom_histogram( binwidth=30, fill="sienna", color="#e9ecef", alpha=0.9) +
+  ggtitle("Census Tracts with 'X' Num. of Households with no cars") +
+  xlab("Number of Households Without a Car") +
+  ylab("# of Census Tracts") +
+  theme_ipsum() +
+  theme(
+    plot.title = element_text(size=10)
+  )
+Hist_NoVehAvail
+
+
+
+
+myvars_acs <-load_variables(2021, "acs5")
+
+
+
+
+### Reference Space
+
+## devtools::install_github("katiejolly/nationalparkcolors")
 
