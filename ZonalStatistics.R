22215ff345f1e6bcecffbf15731409a01c35d526
Your Namesage.gray4@gmail.com <sage.gray4@gmail.com>
2023-01-30 21:19:15 -0500

    Creation of Dot Density and Scatterplot Graphs

--- ZonalStatistics.R
+++ ZonalStatistics.R
@@ -7,7 +7,7 @@ library(sf)
 library(ggplot2)
 library(hrbrthemes)
 library(devtools)
-# library(nationalparkcolors)
+library(viridis)
 library(ggthemes)
 library(RColorBrewer)
 
@@ -103,6 +103,23 @@ zones <- left_join(census, lehd_tracts) %>%
 ### Chloropleths
 chlor_pal <- brewer.pal(5, "BrBG")
 chlor_pal_grey <- brewer.pal(5,"Greys" )
+chlor_pal_greens <- brewer.pal(5, "BuGn")
+
+#Median Income Map
+  ## Data
+  median_income_map <- ggplot(zones) +
+  geom_sf(aes(fill = median_incomeE)) +
+  scale_fill_gradientn(colors = chlor_pal_greens, name = "Median Income")+
+  ggtitle("Median Income") +
+  
+  ##Label Football Stadium
+      geom_point(aes(x=-97.444191, y=35.205841),
+                 color = 'black', size = 4, shape =18) +
+      geom_text(aes(x=-97.20, y=35.205841, label = "football stadium"), size =2, color ="black")
+ 
+    ## Plot Map
+    median_income_map 
+
 
 hh_size_map <- ggplot(zones) +
   geom_sf(aes(fill = total_hhsE)) +
@@ -128,6 +145,7 @@ total_employment_map
 
 
 hh_size_map
+
 ### Historgrams
 hist_hhs <- zones %>%
   ggplot( aes(x=total_hhsE)) +
@@ -146,8 +164,7 @@ Hist_NoVehAvail <- zones %>%
   ggplot( aes(x=no_vehE)) +
   geom_histogram( binwidth=30, fill="sienna", color="#e9ecef", alpha=0.9) +
   ggtitle("Census Tracts with 'X' Num. of Households with no cars") +
-  xlab("Number of Households Without a Car") +
-  ylab("# of Census Tracts") +
+
   theme_ipsum() +
   theme(
     plot.title = element_text(size=10)
@@ -155,13 +172,75 @@ Hist_NoVehAvail <- zones %>%
 Hist_NoVehAvail
 
 
+#ScatterPlot
 
+#Income & Children Living at Home
+
+Living_At_Home_By_Income <- ggplot(zones, aes(x=total_18to34E, y=median_incomeE, color= no_vehE)) + 
+  geom_point(size=3) +
+  xlab("Number of adults living with parents per census tract") +
+  ylab("Median income for census tract") +
+  theme_ipsum()
+
+Living_At_Home_By_Income
 
 myvars_acs <-load_variables(2021, "acs5")
 
 
+# Dot Density Map
+##Create Points
+no_veh_pct <- st_sample(zones, 
+                            size = ceiling(zones$no_vehE/100))
 
+one_veh_pts <- st_sample(zones, 
+                            size = ceiling(zones$one_vehE/100))
 
+two_veh_pts <- st_sample(census, 
+                            size = ceiling(zones$two_vehE/100))
+
+three_veh_pts <- st_sample(census, 
+                            size = ceiling(zones$three_vehE/100))
+fourPlus_veh_pts <- st_sample(zones, 
+                              size = ceiling (zones$fourplus_vehE/100))
+
+## Create Df of Dots
+no_veh_df <- tibble(vehOwn = rep("No Vehicles", 
+                                      length(no_veh_pct))) %>%
+  st_sf(geom = no_veh_pct)
+
+one_veh_df <- tibble(vehOwn = rep("One Vehicle", 
+                                      length(one_veh_pts))) %>%
+  st_sf(geom = one_veh_pts)
+
+two_veh_df <- tibble(vehOwn = rep("Two Vehicles", 
+                                      length(two_veh_pts))) %>%
+  st_sf(geom = two_veh_pts)
+
+three_veh_df <- tibble(vehOwn = rep("Three Vehicles", 
+                                      length(three_veh_pts))) %>%
+  st_sf(geom = three_veh_pts)
+
+four_veh_df <- tibble(vehOwn = rep("Four or More Vehicles", 
+                                   length(fourPlus_veh_pts))) %>%
+  st_sf(geom = fourPlus_veh_pts)
+
+ vehOwn_df<- rbind(no_veh_df, one_veh_df, two_veh_df, three_veh_df, four_veh_df)
+
+ 
+ ## Plot
+ 
+ ggplot(zones) + 
+   geom_sf(color = "white") +
+   geom_sf(data = vehOwn_df, 
+           aes(color = vehOwn), 
+           alpha = 0.3,
+           size = 0.1) +
+   scale_color_brewer("Vehicles Owned\n(each points represents\n100 households)",
+                      palette = "Set1") +
+   theme_void()  +
+   guides(color = guide_legend(override.aes = list(size=5, alpha = 0.6)))
+ 
+ 
 ### Reference Space
 
 ## devtools::install_github("katiejolly/nationalparkcolors")
