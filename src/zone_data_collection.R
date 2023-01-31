214d6beaef4edbae41bf7a9f2ce1c6a605874f4d
Nur Shlapobersky <nur.shlapo@gmail.com>
2023-01-31 16:23:18 -0500

    splitting data collection and visualization, reformatting visualization with reusable components

---
+++ src/zone_data_collection.R
@@ -0,0 +1,152 @@
+#### Load Libraries ####
+library(tidyverse)
+library(tidycensus)
+library(tigris)
+library(here)
+library(sf)
+
+#### Get Employment Data from LEHD ####
+# Download longitudinal employer-household dynamic dataset
+lehd_blocks <-
+  read_csv(
+    'https://lehd.ces.census.gov/data/lodes/LODES7/ok/wac/ok_wac_S000_JT00_2015.csv.gz',
+    show_col_types = FALSE
+  ) %>%
+  replace(is.na(.), 0) %>%
+  rename(total_emp = C000) %>%
+  mutate(basic_emp = CNS01 + CNS02 + CNS03 + CNS04 + CNS05 + CNS06 + CNS08 +
+           CNS09) %>%
+  rename(retail_emp = CNS07) %>%
+  mutate(service_emp = total_emp - basic_emp - retail_emp) %>%
+  select(w_geocode, total_emp, basic_emp, retail_emp, service_emp)
+
+# Collapse data from block to tract level
+lehd_tracts <- lehd_blocks %>%
+  mutate(w_geocode = as.character(w_geocode)) %>%
+  mutate(GEOID = substr(w_geocode, 1, 11)) %>%
+  select(-w_geocode) %>%
+  group_by(GEOID) %>%
+  summarize(across(everything(), ~ sum(.)))
+
+#### Get Household Data from American Community Survey ####
+# Enumerate variables of interest
+hh_vars = c(
+  ## Number of Vehicles
+  no_veh = "B08201_002",
+  total_veh = "B08201_002",
+  one_veh = "B08201_003",
+  two_veh = "B08201_004",
+  three_veh = "B08201_005",
+  fourplus_veh = "B08201_006",
+  
+  ## Total Households
+  total_hhs = "B08201_001",
+  
+  ## Household Structure
+  hh_1person = "B08201_007",
+  hh_2person = "B08201_013",
+  hh_3person = "B08201_019",
+  hh_4person_plus = "B08201_025",
+  
+  ## Children
+  total_under6 = "B05009_002",
+  total_6to17 = "B05009_020",
+  # adults living with parents
+  total_18to34 = "B09021_012",
+  
+  ## Income
+  median_income = "B06011_001",
+  total_household_income = "B19001_001",
+  inc_lt_10k = "B19001_002",
+  inc_btw_10k_15k = "B19001_003",
+  inc_btw_15k_20k = "B19001_004",
+  inc_btw_20k_25k = "B19001_005",
+  inc_btw_25k_30k = "B19001_006",
+  inc_btw_30k_35k = "B19001_007",
+  inc_btw_35k_40k = "B19001_008",
+  inc_btw_40k_45k = "B19001_009",
+  inc_btw_45k_50k = "B19001_010",
+  inc_btw_50k_60k = "B19001_011",
+  inc_btw_60k_75k = "B19001_012",
+  inc_btw_75k_100k = "B19001_013",
+  inc_btw_100k_125k = "B19001_014",
+  inc_btw_125k_150k = "B19001_015",
+  inc_btw_150k_200k = "B19001_016",
+  inc_gt_200k = "B19001_017"
+)
+
+# Get data for OKC counties
+census <- get_acs(
+  geography = "tract",
+  state = "OK",
+  county = c(
+    "Oklahoma",
+    "Lincoln",
+    "Logan",
+    "Canadian",
+    "Grady",
+    "McClain",
+    "Cleveland"
+  ),
+  variables = hh_vars,
+  output = "wide",
+  geometry = TRUE
+) %>%
+  filter(!st_is_empty(geometry)) %>%
+  replace(is.na(.), 0)
+
+#### Get population data from decennial redistricting survey ####
+population <- get_decennial(
+  geography = "tract",
+  state = "OK",
+  year = 2020,
+  county = c(
+    "Oklahoma",
+    "Lincoln",
+    "Logan",
+    "Canadian",
+    "Grady",
+    "McClain",
+    "Cleveland"
+  ),
+  variables = "P1_001N",
+  output = "wide",
+  geometry = FALSE
+) %>%
+  mutate(total_pop = P1_001N) %>%
+  select(c("total_pop", "GEOID")) %>%
+  replace(is.na(.), 0)
+
+#### Use TIGRIS to get land area in sq meters ####
+areas <- tracts(
+  state = "OK",
+  county = c(
+    "Oklahoma",
+    "Lincoln",
+    "Logan",
+    "Canadian",
+    "Grady",
+    "McClain",
+    "Cleveland"
+  )
+) %>%
+  select(GEOID, ALAND) %>%
+  replace(is.na(.), 0) %>%
+  st_drop_geometry()
+
+
+#### Compile data ####
+# Join all datasets
+zones <- left_join(census, lehd_tracts) %>%
+  left_join(areas) %>%
+  left_join(population) %>%
+  
+  ## Calculate densities
+  mutate(emp_density = 1e6 * total_emp / ALAND) %>% # calculate employment density per km2
+  mutate(pop_density = 1e6 * total_pop / ALAND) %>% # calculate population density per km2
+  mutate(act_density = emp_density + pop_density) # calculate activity density per km2
+
+# Export to a geojson
+st_write(zones, here("data", "okc_zone_data.geojson"), append = FALSE, delete_layer = TRUE)
+
+
--- src/zone_visualization.R
+++ src/zone_visualization.R
@@ -1,7 +1,4 @@
-#Load Libraries
-library(tidyverse)
-library(tidycensus)
-library(tigris)
+#### Load Libraries and Data ####
 library(sf)
 library(ggplot2)
 library(hrbrthemes)
@@ -9,224 +6,73 @@ library(devtools)
 library(ggthemes)
 library(RColorBrewer)
 library(treemapify)
+library(here)
 
-# Download longitudinal employer-household dynamic dataset
-lehd_blocks <-
-  read_csv(
-    'https://lehd.ces.census.gov/data/lodes/LODES7/ok/wac/ok_wac_S000_JT00_2015.csv.gz',
-    show_col_types = FALSE
-  ) %>%
-  rename(total_emp = C000) %>%
-  mutate(basic_emp = CNS01 + CNS02 + CNS03 + CNS04 + CNS05 + CNS06 + CNS08 +
-           CNS09) %>%
-  rename(retail_emp = CNS07) %>%
-  mutate(service_emp = total_emp - basic_emp - retail_emp) %>%
-  select(w_geocode, total_emp, basic_emp, retail_emp, service_emp)
+# Load data created in zone_data_collection.R
+zones <- st_read(here("data", "okc_zone_data.geojson"))
 
-# Collapse data from block to tract level
-lehd_tracts <- lehd_blocks %>%
-  mutate(w_geocode = as.character(w_geocode)) %>%
-  mutate(GEOID = substr(w_geocode, 1, 11)) %>%
-  select(-w_geocode) %>%
-  group_by(GEOID) %>%
-  summarize(across(everything(), ~ sum(.)))
-
-# Download census tract data
-hh_vars = c(
-  ## Number of Vehicles
-  no_veh = "B08201_002",
-  total_veh = "B08201_002",
-  one_veh = "B08201_003",
-  two_veh = "B08201_004",
-  three_veh = "B08201_005",
-  fourplus_veh = "B08201_006",
-  
-  ## Total Households
-  total_hhs = "B08201_001",
-  
-  ## Household Structure
-  hh_1person = "B08201_007",
-  hh_2person = "B08201_013",
-  hh_3person = "B08201_019",
-  hh_4person_plus = "B08201_025",
-  
-  ## Children
-  total_under6 = "B05009_002",
-  total_6to17 = "B05009_020",
-  # adults living with parents
-  total_18to34 = "B09021_012",
-  
-  ## Income
-  total_household_income = "B19001_001",
-  inc_lt_10k = "B19001_002",
-  inc_btw_10k_15k = "B19001_003",
-  inc_btw_15k_20k = "B19001_004",
-  inc_btw_20k_25k = "B19001_005",
-  inc_btw_25k_30k = "B19001_006",
-  inc_btw_30k_35k = "B19001_007",
-  inc_btw_35k_40k = "B19001_008",
-  inc_btw_40k_45k = "B19001_009",
-  inc_btw_45k_50k = "B19001_010",
-  inc_btw_50k_60k = "B19001_011",
-  inc_btw_60k_75k = "B19001_012",
-  inc_btw_75k_100k = "B19001_013",
-  inc_btw_100k_125k = "B19001_014",
-  inc_btw_125k_150k = "B19001_015",
-  inc_btw_150k_200k = "B19001_016",
-  inc_gt_200k = "B19001_017"
+#### Create reusable themes ####
+# Create clear theme for maps
+clear_map_theme <- theme(
+  axis.text = element_blank(),
+  axis.ticks = element_blank(),
+  panel.background = element_blank(),
+  panel.grid.major = element_blank(),
+  panel.grid.minor = element_blank(),
+  title = element_blank()
 )
 
-census <- get_acs(
-  geography = "tract",
-  state = "OK",
-  county = c(
-    "Oklahoma",
-    "Lincoln",
-    "Logan",
-    "Canadian",
-    "Grady",
-    "McClain",
-    "Cleveland"
-  ),
-  variables = hh_vars,
-  output = "wide",
-  geometry = TRUE
-) %>%
-  filter(!st_is_empty(geometry))
+# Create football stadium element
+football_point <- geom_point(
+  aes(x = -97.444191, y = 35.205841),
+  color = 'black',
+  size = 4,
+  shape = 18
+)
+football_text <- geom_text(
+  aes(x = -97.20, y = 35.205841, label = "Football Stadium"),
+  size = 2,
+  color = "black"
+)
 
-## Get the land area in sq meters
-areas <- tracts(
-  state = "OK",
-  
-  county = c(
-    "Oklahoma",
-    "Lincoln",
-    "Logan",
-    "Canadian",
-    "Grady",
-    "McClain",
-    "Cleveland"
-  )
-) %>%
-  select(GEOID, ALAND) %>%
-  st_drop_geometry()
-
-
-## Get population data
-population <- get_decennial(
-  geography = "tract",
-  state = "OK",
-  year = 2020,
-  county = c(
-    "Oklahoma",
-    "Lincoln",
-    "Logan",
-    "Canadian",
-    "Grady",
-    "McClain",
-    "Cleveland"
-  ),
-  variables = "P1_001N",
-  output = "wide",
-  geometry = FALSE
-) %>%
-  mutate(total_pop = P1_001N) %>%
-  select(c("total_pop", "GEOID"))
-
-## Join datasets and calculate densities
-zones <- left_join(census, lehd_tracts) %>%
-  left_join(areas) %>%
-  left_join(population) %>%
-  mutate(emp_density = 1e6 * total_emp / ALAND) %>% # calculate employment density per km2
-  mutate(pop_density = 1e6 * total_pop / ALAND) %>% # calculate population density per km2
-  mutate(act_density = emp_density + pop_density) # calculate activity density per km2
-
-## Export to a geojson
-st_write(zones, "okc_zone_data.geojson", append = FALSE)
-
-
-### Chloropleths
+#### Create Chloropleth Maps ####
 chlor_pal <- brewer.pal(5, "BrBG")
 chlor_pal_grey <- brewer.pal(5, "Greys")
 chlor_pal_greens <- brewer.pal(5, "BuGn")
 
-#Median Income Map
+# Median Income Map
 ## Data
-median_income_map <- ggplot(zones) +
+ggplot(zones) +
   geom_sf(aes(fill = median_incomeE)) +
   scale_fill_gradientn(colors = chlor_pal_greens, name = "Median Income") +
   
   ##Label Football Stadium
-  geom_point(
-    aes(x = -97.444191, y = 35.205841),
-    color = 'black',
-    size = 4,
-    shape = 18
-  ) +
-  geom_text(
-    aes(x = -97.20, y = 35.205841, label = "Football Stadium"),
-    size = 2,
-    color = "black"
-  ) +
-  theme(
-    axis.text = element_blank(),
-    axis.ticks = element_blank(),
-    panel.background = element_blank(),
-    panel.grid.major = element_blank(),
-    panel.grid.minor = element_blank(),
-    legend.title = element_text("Median Income"),
-    title = element_blank()
-  )
-
-## Plot Map
-median_income_map
+  football_point +
+  football_text +
+  
+  theme(legend.title = element_text("Median Income")) +
+  clear_map_theme
 
 # Number of Households
-hh_size_map <- ggplot(zones) +
+ggplot(zones) +
   geom_sf(aes(fill = total_hhsE)) +
   scale_fill_gradientn(colors = chlor_pal, name = "# of Households") +
   ggtitle("Number of Households") +
   theme(plot.title = element_text(size = 12)) +
-  geom_point(
-    aes(x = -97.444191, y = 35.205841),
-    color = 'black',
-    size = 4,
-    shape = 18
-  ) +
-  # geom_label(aes(x=-97.444191, y=35.205841, label= "Football Stadium"))
-  geom_text(
-    aes(x = -97.20, y = 35.205841, label = "Football Stadium"),
-    size = 2,
-    color = "black"
-  ) +
-  theme(
-    axis.text = element_blank(),
-    axis.ticks = element_blank(),
-    panel.background = element_blank(),
-    panel.grid.major = element_blank(),
-    panel.grid.minor = element_blank(),
-    legend.title = element_text("Median Income"),
-    title = element_blank()
-  )
-
-hh_size_map + scale_colour_solarized()
+  football_point +
+  football_text +
+  theme(legend.title = element_text("Median Income")) +
+  clear_map_theme +
+  scale_colour_solarized()
 
 
 # Total Employment
-total_employment_map <- ggplot(zones) +
+ggplot(zones) +
   geom_sf(aes(fill = total_emp), color = NA) +
   ggtitle("Total Employment") +
   theme(plot.title = element_text(size = 12))  +
-  theme(
-    axis.text = element_blank(),
-    axis.ticks = element_blank(),
-    panel.background = element_blank(),
-    panel.grid.major = element_blank(),
-    panel.grid.minor = element_blank(),
-    legend.title = element_text("Median Income"),
-    title = element_blank()
-  )
-total_employment_map
+  theme(legend.title = element_text("Median Income")) +
+  clear_map_theme
 
 
 hh_size_map
@@ -298,7 +144,7 @@ activity_density_map
 
 
 
-### Histograms
+#### Create Histograms ####
 
 
 hist_hhs <- zones %>%
@@ -430,27 +276,6 @@ ggplot(zones) +
                      palette = "Set1") +
   theme_void()  +
   guides(color = guide_legend(override.aes = list(size = 5, alpha = 0.6)))
-
-
-### Reference Space
-
-## Attempts to get a Tree Map of Employment
-# emp_vars <- c(`Basic Employees` = zones$basic_emp,
-#           `Retail Employees` = zones$retail_emp,
-#           `Service Employees` = zones$service_emp)
-
-# treemap_data <- zones %>%
-#   select(c(basic_emp, retail_emp, service_emp))
-#
-# summarise(treemap_data, )
-#
-# ggplot(treemap_data, aes(tree = (), fill= basic_emp)) +
-# geom_treemap(show.legend = FALSE, color = NA) +
-# geom_treemap_text(aes(label = paste(variable, "\n",
-#                                       prettyNum(pct * 100, digits = 1),
-#                                        "%",sep = "")),
-#                      color = "white") +
-#    scale_fill_brewer(palette = "Set2")
 
 
 
